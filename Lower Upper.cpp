d950a965aa370d9d993aaa44228067d87b2c03ac
Soumil Shergill <106508095+SoumilShergill@users.noreply.github.com>
2022-05-30 12:19:06 +0530

    Update Lower Upper.cpp

--- Lower Upper.cpp
+++ Lower Upper.cpp
@@ -1,4 +1,13 @@
 #include<iostream>
-int main() {
-	return 0;
+ using namespace std;
+ int main(){  
+char ch;
+cin>>ch;
+ if(ch>=65&&ch<=90)
+cout<<endl<<"UPPERCASE";
+else if(ch>=97&&ch<=122)
+cout<<endl<<"lowercase";
+else
+cout<<endl<<"Invalid";
+return 0;
 }
