8b53c3284b5c4d914b8bd767dd83763f3f705240
Soumil Shergill <106508095+SoumilShergill@users.noreply.github.com>
2022-05-30 12:06:31 +0530

    Create Arrays-Insertion Sort.cpp

---
+++ Arrays-Insertion Sort.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+class node {
+public:
+	int data;
+	node *next;
+	node(int d)
+	{
+		data = d;
+		next = NULL;
+	}
+};
+
+node* input(int N)
+{	int d;
+	node *head = NULL, *p;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> d;
+		if (head == NULL)
+		{
+			head = new node(d);
+			p = head;
+		}
+		else
+		{
+			p->next = new node(d);
+			p = p->next;
+		}
+	}
+	return head;
+}
+
+void sort(node *head, int N)
+{	node *ahead = head->next;
+	while (ahead != NULL)
+	{
+		node *cur = head;
+		while ( cur != ahead ) 
+		{
+			if (cur->data > ahead->data)
+			{
+				int temp = cur->data;
+				cur->data = ahead->data;
+				ahead->data = temp;
+			}
+			cur = cur->next;
+		
+
+		}
+		ahead = ahead->next;
+	}
+	node *p = head;
+	for (int i = 0; i < N; ++i)
+	{
+		cout << p->data << " ";
+		p = p->next;
+	}
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+	node *head = input(N);
+	sort(head, N);
+}
